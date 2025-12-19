4cad41508aea92429dcf6c3b5b17c66e8bdbae0d
huchen2000 <huchen2000@gmail.com>
2025-12-18 21:45:37 -0500

    dockerization

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Ensure environment variables and directories (like prisma/data) exist
+npm run ensure:env
+
 # Run prisma db push to ensure schema matches
 npx prisma db push
 
